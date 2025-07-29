efc081e9da12df2aa5a28863b61d5a26287a5445
Callum Saunders <callumsaunders@Callums-MacBook-Pro.local>
2025-07-29 22:15:41 +0100

    Add simple venue test to debug 500 error

--- js/event-store.js
+++ js/event-store.js
@@ -157,14 +157,15 @@ class EventAPI {
   }
 
   async getVenues() {
-    const response = await fetch(`${this.baseUrl}/get-venue-list-firestore`);
+    const response = await fetch(`${this.baseUrl}/simple-venue-test`);
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     
     const data = await response.json();
-    return data || [];
+    console.log('Simple test response:', data);
+    return data.venues || [];
   }
 }
 
